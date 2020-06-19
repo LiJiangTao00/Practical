35b41719e945a6f29f1382c6bda6727f7396ddd7
涛 <@LAPTOP-F38H79UL>
2020-06-19 18:32:49 +0800

    ddd

---
+++ PC.IBLL/UserIBLL/IUBLL.cs
@@ -0,0 +1,17 @@
+﻿using PC.Model.Models;
+using PC.Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PC.IBLL.UserIBLL
+{
+    public interface IUBLL
+    {
+        int Login(string user_Phone, string user_Pwd);
+        int Forget(UserTableModel m);
+        string SelectName(string user_Phone);
+        List<DepartmentTableModel> ShowDepartment();
+        List<UserShowModel> ShowUser();
+    }
+}
