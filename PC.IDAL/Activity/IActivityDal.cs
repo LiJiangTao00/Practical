de48249a0376344cb36c3e8e6e68f4c1dc47b9ac
lenovo <lenovo@JULIA>
2020-06-19 10:09:52 +0800

    1111

---
+++ PC.IDAL/Activity/IActivityDal.cs
@@ -0,0 +1,33 @@
+﻿using PC.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PC.IDAL.Activity
+{
+    public interface IActivityDal
+    {
+        /// <summary>
+        /// 显示活动列表
+        /// </summary>
+        /// <returns></returns>
+        List<Activity_DoTableModel> ShowActivity();
+        /// <summary>
+        /// 添加活动
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <returns></returns>
+        int AddActivity(ActivityTableModel activity);
+        /// <summary>
+        /// 显示活动状态
+        /// </summary>
+        /// <returns></returns>
+        List<ActivityStateTableModel> ShowActivityState();
+        /// <summary>
+        /// 显示活动类型
+        /// </summary>
+        /// <returns></returns>
+        List<ActivityTypeTableModel> ShowActivityType();
+    }
+}
