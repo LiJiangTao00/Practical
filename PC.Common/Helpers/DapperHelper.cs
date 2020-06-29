1ec103a4f7da3ad5cb8f198ee2af1bfec9bff414
平凡岁月星星辰 <@DESKTOP-TIMIH6O>
2020-06-29 17:14:49 +0800

    会议 20200629 17.14

--- PC.Common/Helpers/DapperHelper.cs
+++ PC.Common/Helpers/DapperHelper.cs
@@ -26,5 +26,12 @@ namespace PC.Common.Helpers
                 return res;
             }
         }
+        public T ShowProc(string proc, DynamicParameters par)
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=192.168.1.117;Initial Catalog=Practial;User ID=sa;Pwd=12345"))
+            {
+                return con.Query<T>(proc, par, commandType: System.Data.CommandType.StoredProcedure).FirstOrDefault();
+            }
+        }
     }
 }
