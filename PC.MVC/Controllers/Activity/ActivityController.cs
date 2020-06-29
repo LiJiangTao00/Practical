27216821e9778148e657b55563086b05068615a0
lenovo <lenovo@JULIA>
2020-06-29 20:54:24 +0800

    get

---
+++ PC.MVC/Controllers/Activity/ActivityController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PC.MVC.Controllers.Activity
+{
+    public class ActivityController : Controller
+    {
+        public IActionResult ShowActivity()
+        {
+            return View();
+        }
+        public IActionResult AddActivity()
+        {
+            return View();
+        }
+        public IActionResult UpdActivity()
+        {
+            return View();
+        }
+        public IActionResult Details(int id=-1)
+        {
+            ViewBag.show = id;
+            return View();
+        }
+        public IActionResult Execute()
+        {
+            return View();
+        }
+    }
+}
